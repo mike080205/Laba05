9d1b141ba79b0cec5d8f17c1f4e2ad53c2c99d63
mike080205 <ijboldin.misha2005@gmail.com>
2024-05-20 23:33:26 +0300

    Додав роботу

---
+++ Lab05App/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Lab05Lib;
+
+namespace Lab05App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть дані планшета:");
+            Console.Write("Модель: ");
+            string model = Console.ReadLine();
+            Console.Write("Виробник: ");
+            string manufacturer = Console.ReadLine();
+            Console.Write("Розмір екрану (в дюймах): ");
+            double screenSize = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Об'єм пам'яті (в ГБ): ");
+            int memorySize = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ємність акумулятора (мАг): ");
+            int batteryCapacity = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Має Wi-Fi (true/false): ");
+            bool hasWiFi = Convert.ToBoolean(Console.ReadLine());
+            Console.Write("Має 4G (true/false): ");
+            bool has4G = Convert.ToBoolean(Console.ReadLine());
+            Console.Write("Ціна: грн");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Tablet tablet = new Tablet(model, manufacturer, screenSize, memorySize, batteryCapacity, hasWiFi, has4G, price);
+
+            Console.WriteLine("\nДеталі планшета:");
+            Console.WriteLine($"Модель: {tablet.Model}");
+            Console.WriteLine($"Виробник: {tablet.Manufacturer}");
+            Console.WriteLine($"Розмір екрану: {tablet.ScreenSize} дюймів");
+            Console.WriteLine($"Розмір пам'яті: {tablet.MemorySize} GB");
+            Console.WriteLine($"Ємність батареї: {tablet.BatteryCapacity} mAh");
+            Console.WriteLine($"Є WiFi: {tablet.HasWiFi}");
+            Console.WriteLine($"Має 4G: {tablet.Has4G}");
+            Console.WriteLine($"Ціна: {tablet.Price} грн");
+            Console.WriteLine($"Ціна за мАг: ${tablet.CalculatePricePermAh():0.00}");
+
+            Console.WriteLine("\nЩоб вийти, натисніть будь-яку клавішу.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab05Lib/Class1.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab05Lib
+{
+    public class Tablet
+    {
+        // Поля класу
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public double ScreenSize { get; set; }
+        public int MemorySize { get; set; }
+        public int BatteryCapacity { get; set; }
+        public bool HasWiFi { get; set; }
+        public bool Has4G { get; set; }
+        public double Price { get; set; }
+
+        // Конструктор класу
+        public Tablet(string model, string manufacturer, double screenSize, int memorySize, int batteryCapacity, bool hasWiFi, bool has4G, double price)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            ScreenSize = screenSize;
+            MemorySize = memorySize;
+            BatteryCapacity = batteryCapacity;
+            HasWiFi = hasWiFi;
+            Has4G = has4G;
+            Price = price;
+        }
+
+        // Метод класу для обчислення ціни за 1 мАг батареї
+        public double CalculatePricePermAh()
+        {
+            if (BatteryCapacity != 0)
+                return Price / BatteryCapacity;
+            else
+                return 0;
+        }
+    }
+}
